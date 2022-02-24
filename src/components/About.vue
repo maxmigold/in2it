b9714d60617b7bb40a6fec61fcfa0da3a8237038
christopher <tomiwa_chris@yahoo.com>
2022-02-24 09:38:36 +0100

    footer section seems completed

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,16 @@
 <template>
-<v-main>
-   This is the About page
- </v-main>
+  <div class="contain">
+    <v-img
+  height="690"
+  width="100%"
+  src="https://picsum.photos/id/11/500/300"
+></v-img>
+  </div>
+
+
+
+
+   
 </template>
 <script>
 export default {
@@ -17,3 +26,14 @@ export default {
  
 };
 </script>
+<style scoped>
+.contain{
+  
+background: #D9D5D5;
+background: -webkit-linear-gradient(top left, #D9D5D5, #088AA4);
+background: -moz-linear-gradient(top left, #D9D5D5, #088AA4);
+background: linear-gradient(to bottom right, #D9D5D5, #088AA4);;
+height: 100%;
+width: 100%;
+}
+</style>
